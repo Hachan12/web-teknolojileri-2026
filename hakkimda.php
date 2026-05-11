0ea714ef35f70ef222a2f2d8051218e4b83fd04b
Hachan12 <hasanaraboga@outlook.com>
2026-05-12 01:17:50 +0300

    day 9

---
+++ hakkimda.php
@@ -0,0 +1,197 @@
+﻿<?php
+session_start();
+if (!isset($_SESSION['UserData']['email'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Web Teknolojisi Projesi</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="css/home.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/clock.css" rel="stylesheet">
+</head>
+
+<body class="overflow-auto" style="background-image: url('images/arkaplan.jpg')">
+
+    <header class="py-0">
+        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-warning p-1 md-auto">
+            <div class="container-fluid shadow p-3 mb-4 rounded">
+                <a class="navbar-brand" href="https://cs.sakarya.edu.tr/" target="_blank">
+                    <img src="images/SakaryaUniversitesi-logo.png" alt="" width="40" height="54"
+                        class="d-inline-block align-text-bottom">
+
+                </a>
+
+                <div class="navbar-brand">Hasan Yahya Araboğa
+                    <a href="logout.php">
+                        <img src="images/logout.png" alt="Çıkış Yap.." width="24px" data-bs-toggle="tooltip">
+                    </a>
+                    <br>Web Teknolojisi Projesi
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link p-2 p-lg-3 active" aria-current="page"
+                                href="hakkimda.php">Hakkımda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link p-2 p-lg-3" href="cv.php">CV</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link p-2 p-lg-3" href="ilgialan.php">İlgi alanlarım</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link p-2 p-lg-3" href="sehir.php">Yaşadığım Şehir</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link p-2 p-lg-3" href="miras.php">Miras</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link p-2 p-lg-3" href="iletisim.php">İletişim</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="flex-wrap container justify-content-around p-5">
+        <div class="row justify-content-around">
+            <div class="col"></div>
+
+            <div class="col-md-auto">
+                <div class="clock-container">
+                    <div class="clock">
+                        <div class="needle hour"></div>
+                        <div class="needle minute"></div>
+                        <div class="needle second"></div>
+                        <div class="center-point"></div>
+                    </div>
+
+                    <div class="time"></div>
+                    <div class="date"></div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow rounded">
+                    <div class="card-img-overlay">
+                        <img src="images/cyaPin.png" width="24px" alt="...">
+                    </div>
+                    <h4 class="card-title text-primary text-center">Hasan Yahya Araboğa</h4>
+
+                    <img src="images/profil.jpg" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+
+                        <p class="card-text">Sakarya Üniversitesi'nde bir öğrenci,<br> Türkiye Sakarya,<br>
+                            Bilgisayar ve Bilişim Bilimleri Fakültesi <br>
+                            Bilgisayar Mühendisliği Bölümü</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Doğum tarihi: 14/07/2006</li>
+                        <li class="list-group-item">Doğduğu Ülke: <a
+                                href="https://tr.wikipedia.org/wiki/Türkiye" target="_blank">Türkiye</a>
+                        </li>
+                        <li class="list-group-item">Yaşadığım Şehir: <a href="https://tr.wikipedia.org/wiki/Sakarya"
+                                target="_blank">Sakarya</a> </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow rounded">
+                    <div class="card-img-overlay">
+                        <img src="images/cyaPin.png" width="24px" alt="...">
+                    </div>
+                    <h3 class="text-primary p-2">Hobilerim</h3>
+                    <div class="card-body">
+
+                        <p class="card-text">Boş Zamanlarımda Yapmaktan Hoşlandığım Şeyler
+                        </p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <img src="images/tekL.png" width="48px" alt="...">
+                            <a href="https://www.btk.gov.tr/bilgi-teknolojileri" target="_blank">IT Çalışmaları</a>
+                        </li>
+                        <li class="list-group-item">
+                            <img src="images/sporL.png" width="48px" alt="...">
+                            <a href="https://blog.skechers.com.tr/sporun-faydalari/" target="_blank">Spor</a>
+                        </li>
+                        <li class="list-group-item">
+                            <img src="images/vgL.png" width="48px" alt="...">
+                            <a href="https://bilimgenc.tubitak.gov.tr/makale/video-oyunlari-kim-ne-zaman-icat-etti"
+                                target="_blank">Video oyunları</a>
+                        </li>
+                        <li class="list-group-item">
+                            <img src="images/movL.png" width="48px" alt="...">
+                            <a href="https://www.themoviedb.org/" target="_blank">Filmler</a>
+                        </li>
+                    </ul>
+                </div>
+                <br>
+                <div class="card shadow rounded">
+                    <div class="card-img-overlay">
+                        
+                    </div>
+                    <h3 class="text-primary p-2">Ulaşın..</h3>
+                    <div class="card-body">
+
+                        <p class="card-text">
+                            Bana ulaşın, ikimiz de bizim için faydalı bir şeyler paylaşabiliriz.
+                        </p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <img src="images/wappL.png" width="48px" alt="...">
+                            <a href="tel:05441173212">+90 5441173212</a>
+                        </li>
+                        <li class="list-group-item">
+                            <img src="images/gmailL.png" width="48px" alt="...">
+                            <a href="mailto:hasanaraboga@hotmail.com"
+                                target="_blank">hasanaraboga@hotmail.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="progress-bar-top">
+        <button class="back-to-top hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" class="back-to-top-icon" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11l5-5m0 0l5 5m-5-5v12" />
+            </svg>
+        </button>
+    </div>
+     <footer class="bg-dark bg-gradient shadow text-white text-center mt-5 p-3">
+        &copy; 2026 Hasan Yahya Araboğa - <a href="#" class="text-white-50">GitHub</a> | <a href="#"
+            class="text-white-50">Instagram</a> | <a href="#" class="text-white-50">Twitter</a>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
+    <script src="js/clock.js"></script>
+    <script src="js/scroll.js"></script>
+</body>
+
+</html>
